d8f9655edb34cec6b3cf24f2489f0fcfc0ad81c8
刘巨 <liuju150@outlook.com>
2021-03-05 17:32:53 +0800

    patch

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -51,7 +51,7 @@ const user = {
     GetInfo ({ commit }) {
       return new Promise((resolve, reject) => {
         getInfo().then(response => {
-          const result = response.result
+          const result = response.Data
 
           if (result.role && result.role.permissions.length > 0) {
             const role = result.role
