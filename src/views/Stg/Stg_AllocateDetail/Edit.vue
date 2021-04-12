e7c12c1a536d0527b88717f7bc096d32fecdf9b0
刘巨 <liuju150@outlook.com>
2021-04-12 10:18:35 +0800

    发货策略完成

---
+++ src/views/Stg/Stg_AllocateDetail/Edit.vue
@@ -0,0 +1,107 @@
+<template>
+  <a-modal :title="title" :width="640" :visible="visible" :confirmLoading="loading" @ok="handleSubmit" @cancel="()=>{this.visible=false}">
+    <a-spin :spinning="loading">
+      <a-form-model ref="form" :model="entity" :rules="rules" v-bind="layout">
+        <a-form-model-item label="编号" prop="Code">
+          <a-input v-model="entity.Code" autocomplete="off" />
+        </a-form-model-item>
+        <a-form-model-item label="名称" prop="Name">
+          <a-input v-model="entity.Name" autocomplete="off" />
+        </a-form-model-item>
+        <a-form-model-item label="配货单位" prop="AllocUOM">
+          <EnumSelect code="Unit" v-model="entity.AllocUOM"></EnumSelect>
+        </a-form-model-item>
+        <a-form-model-item label="配货代码" prop="PickBusCode">
+          <EnumSelect code="Stg_AllocateDetail_PickBusCode" v-model="entity.PickBusCode"></EnumSelect>
+        </a-form-model-item>
+        <a-form-model-item label="库位类型" prop="LocType">
+          <EnumSelect code="Bas_Loc_Type" v-model="entity.LocType" :allowClear="true"></EnumSelect>
+        </a-form-model-item>
+      </a-form-model>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import MainSvc from '@/api/Stg/Stg_AllocateDetailSvc'
+import EnumSelect from '@/components/CF/EnumSelect'
+import CodeInput from '@/components/CF/CodeInput'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import TreeSelect from '@/components/CF/TreeSelect'
+import CommonSelect from '@/components/CF/CommonSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+export default {
+  components: {
+    MainSvc,
+    CodeInput,
+    EnumSelect,
+    StorerSelect,
+    TreeSelect,
+    CommonSelect,
+    LocSelect
+  },
+  props: {},
+  data() {
+    return {
+      title: '新建',
+      layout: { labelCol: { xs: { span: 24 }, sm: { span: 6 } }, wrapperCol: { xs: { span: 24 }, sm: { span: 14 } } },
+      rules: {
+        Name: [{ required: true, message: '必填' }],
+        Code: [{ required: true, message: '必填' }]
+      },
+      visible: false,
+      loading: false,
+      entity: {},
+      parentEntity: {}
+    }
+  },
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    })
+  },
+  created() { },
+  methods: {
+    ...mapActions({ getConfig: 'getConfig' }),
+    init() {
+      this.loading = false
+      this.visible = true
+      // 这里的ParentId为具体业务主表Id
+      this.entity = { Id: '', WhseId: this.defaultWhseId, AllocateStgId: this.parentEntity.Id, Code: '', Name: '', AllocUOM: 'Tray', PickBusCode: 'Default', LocType: '' }
+      this.$nextTick(() => {
+        this.$refs.form.clearValidate()
+      })
+    },
+    openForm(id, main, title) {
+      this.title = title || (id ? '新建' : '修改')
+      this.init()
+      this.parentEntity = main
+      if (id) {
+        MainSvc.Get(id).then(resJson => {
+          this.entity = resJson.Data
+        })
+      }
+    },
+    handleSubmit() {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.loading = true
+        MainSvc.Save(this.entity).then(result => {
+          this.loading = false
+          if (result.Success) {
+            this.$message.success(result.Msg)
+            this.visible = false
+            this.$emit('Success')
+          } else {
+            this.$message.error(result.Msg)
+          }
+        })
+      })
+    }
+  }
+}
+</script>
--- src/views/Stg/Stg_AllocateDetail/List.vue
+++ src/views/Stg/Stg_AllocateDetail/List.vue
@@ -1,61 +1,47 @@
 <template>
-  <a-card :bordered="false">
-    <div class="table-page-search-wrapper">
-      <a-form layout="inline">
-        <a-row :gutter="48">
-          <a-col :md="6" :sm="24">
-            <a-form-item label="关键字">
-              <a-input v-model="queryParam.Keyword" placeholder="关键字" />
-            </a-form-item>
-          </a-col>
-          <a-col :md="6" :sm="24">
-            <span class="table-page-search-submitButtons">
-              <a-button type="primary" v-action:Query @click="()=>{this.$refs.table.refresh()}">查询</a-button>
-              <a-button style="margin-left: 8px" @click="resetSearchForm()">重置</a-button>
-            </span>
-          </a-col>
-        </a-row>
-      </a-form>
-    </div>
-
+  <a-drawer title="明细" placement="right" width="60%" :visible="visible" @close="()=>{this.visible=false}">
     <div class="table-operator">
       <a-button type="primary" v-action:Add icon="plus" @click="handleAdd">新建</a-button>
       <a-button type="primary" v-action:Delete icon="delete" @click="handleDelete()">批量删除</a-button>
     </div>
 
     <s-table ref="table" size="default" rowKey="Id" :columns="columns" :data="loadData" :rowSelection="rowSelection" showPagination="auto">
-      <span slot="ModifyTime" slot-scope="text">
-        {{ moment(text).format("yyyy-MM-DD") }}
-      </span>
+      <template slot="AllocUOM" slot-scope="text">
+        <EnumName code="Unit" :value="text"></EnumName>
+      </template>
+      <template slot="PickBusCode" slot-scope="text">
+        <EnumName code="Stg_AllocateDetail_PickBusCode" :value="text"></EnumName>
+      </template>
+      <template slot="LocType" slot-scope="text">
+        <EnumName code="Bas_Loc_Type" :value="text"></EnumName>
+      </template>
       <span slot="action" slot-scope="text, record">
         <template>
           <a v-action:Update @click="handleEdit(record)">修改</a>
           <a-divider v-action:Delete type="vertical" />
           <a v-action:Delete @click="handleDelete([record])">删除</a>
-          <a-divider v-action:Detail type="vertical" />
-          <a v-action:Detail @click="handleDetail(record)">明细</a>
         </template>
       </span>
     </s-table>
     <EditForm ref="editForm" @Success="()=>{this.$refs.table.refresh()}"></EditForm>
-    <ListDetail ref="listDetail"></ListDetail>
-  </a-card>
+  </a-drawer>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import moment from 'moment'
 import { STable } from '@/components'
-import MainSvc from '@/api/Stg/Stg_AllocateSvc'
+import MainSvc from '@/api/Stg/Stg_AllocateDetailSvc'
 import EditForm from './Edit'
 import EnumSelect from '@/components/CF/EnumSelect'
 import EnumName from '@/components/CF/EnumName'
-import ListDetail from '../Stg_AllocateDetail/List'
 
 const columns = [
   { title: '编号', dataIndex: 'Code', sorter: true },
   { title: '名称', dataIndex: 'Name', sorter: true },
-  { title: '修改时间', dataIndex: 'ModifyTime', sorter: true, scopedSlots: { customRender: 'ModifyTime' } },
+  { title: '配货单位', dataIndex: 'AllocUOM', scopedSlots: { customRender: 'AllocUOM' } },
+  { title: '配货代码', dataIndex: 'PickBusCode', scopedSlots: { customRender: 'PickBusCode' } },
+  { title: '库位类型', dataIndex: 'LocType', scopedSlots: { customRender: 'LocType' } },
   { title: '操作', dataIndex: 'action', width: '200px', scopedSlots: { customRender: 'action' } }
 ]
 
@@ -65,8 +51,7 @@ export default {
     MainSvc,
     EnumSelect,
     EnumName,
-    EditForm,
-    ListDetail
+    EditForm
   },
   data() {
     this.columns = columns
@@ -77,8 +62,8 @@ export default {
       mdl: null,
       // 高级搜索 展开/关闭
       advanced: false,
-      // 查询参数
-      queryParam: { WhseId: '', Keyword: '' },
+      // 查询参数 // 这里的ParentId具体业务主表字段Id
+      queryParam: { WhseId: '', AllocateStgId: '', Keyword: '' },
       // 加载数据方法 必须为 Promise 对象
       loadData: parameter => {
         this.queryParam.WhseId = this.defaultWhseId
@@ -87,7 +72,8 @@ export default {
         return MainSvc.GetPage(requestParameters)
       },
       selectedRowKeys: [],
-      selectedRows: []
+      selectedRows: [],
+      entity: {}
     }
   },
   filters: {
@@ -107,25 +93,30 @@ export default {
   },
   methods: {
     moment,
+    openForm(record) {
+      this.entity = record
+      // 这里的ParentId具体业务主表字段Id
+      this.queryParam.AllocateStgId = record.Id
+      this.visible = true
+      this.nextTick(() => {
+        this.refs.table.refresh()
+      })
+    },
     handleAdd() {
       this.mdl = null
-      this.visible = true
-      this.$refs.editForm.openForm(null, '新增')
+      this.$refs.editForm.openForm(null, this.entity, '新增')
     },
     handleEdit(record) {
-      this.visible = true
       this.mdl = { ...record }
-      this.$refs.editForm.openForm(record.Id, '修改')
-    },
-    handleDetail(record) {
-      this.$refs.listDetail.openForm(record)
+      this.$refs.editForm.openForm(record.Id, this.entity, '修改')
     },
     onSelectChange(selectedRowKeys, selectedRows) {
       this.selectedRowKeys = selectedRowKeys
       this.selectedRows = selectedRows
     },
     resetSearchForm() {
-      this.queryParam = { WhseId: this.defaultWhseId, Keyword: '' }
+      // 这里的ParentId具体业务主表字段Id
+      this.queryParam = { WhseId: this.defaultWhseId, AllocateStgId: this.entity.Id, Keyword: '' }
     },
     handleDelete(rows) {
       var thisObj = this
