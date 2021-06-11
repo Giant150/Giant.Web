3a05a27984e2c6c94d988e4be63a8199cd5de826
Giant <liuju150@outlook.com>
2021-06-11 17:16:09 +0800

    库存调整

---
+++ src/views/Inv/Inv_Adjust/Edit.vue
@@ -0,0 +1,212 @@
+<template>
+  <a-drawer title="库存调整" placement="right" width="60%" :visible="visible" @close="()=>{this.visible=false}" :maskClosable="false" :body-style="{ paddingBottom: '80px' }">
+    <a-spin :spinning="loading">
+      <a-form-model ref="form" :model="entity" :rules="rules" v-bind="layout">
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="货主" prop="StorerId">
+              <StorerSelect v-model="entity.StorerId" :type="['Storer']" placeholder="货主"></StorerSelect>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="编码" prop="Code">
+              <CodeInput ref="codeInput" code="Inv_Adjust_Code" v-model="entity.Code" :para="{}" placeholder="编码"></CodeInput>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="制单日期" prop="DocDate">
+              <a-date-picker v-model="entity.DocDate" valueFormat="YYYY-MM-DD"></a-date-picker>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="8">
+            <a-form-model-item label="调整日期" prop="AdjDate">
+              <a-date-picker v-model="entity.AdjDate" valueFormat="YYYY-MM-DD"></a-date-picker>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="16">
+            <a-form-model-item label="备注" prop="Remark">
+              <a-input v-model="entity.Remark" autocomplete="off" />
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+      <div class="table-operator">
+        <a-button type="primary" v-action:Add icon="plus" @click="handleAdd()">新建</a-button>
+      </div>
+      <a-table ref="table" size="small" rowKey="Id" :columns="columns" :data-source="adjustDetail" :pagination="false">
+        <template slot="QtyAdj" slot-scope="text, record">
+          <a-input-number v-model="record.QtyAdj" style="width:100%" size="small" />
+        </template>
+        <template slot="Type" slot-scope="text, record">
+          <EnumSelect code="Inv_AdjustDetail_Type" v-model="record.Type" style="width:100%" size="small"></EnumSelect>
+        </template>
+        <span slot="action" slot-scope="text, record">
+          <template>
+            <a v-action:Delete @click="handleDelete(record)">删除</a>
+          </template>
+        </span>
+      </a-table>
+      <InvChoose type="radio" ref="invChoose" @choose="handlerInvChoose"></InvChoose>
+    </a-spin>
+    <div :style="{ position: 'absolute', bottom: 0, right: 0, width: '100%', borderTop: '1px solid #e9e9e9', padding: '10px 16px', background: '#fff', textAlign: 'right', zIndex: 1, }">
+      <a-button :style="{ marginRight: '8px' }" type="default" @click="handlePrint">打印调整单</a-button>
+      <a-button :style="{ marginRight: '8px' }" type="primary" @click="handleAdjust">确认调整</a-button>
+      <a-button :style="{ marginRight: '8px' }" type="primary" @click="handleSubmit">保存</a-button>
+      <a-button :style="{ marginRight: '8px' }" @click="()=>{this.visible=false}">关闭</a-button>
+    </div>
+  </a-drawer>
+</template>
+
+<script>
+import moment from 'moment'
+import { mapActions, mapGetters } from 'vuex'
+import MainSvc from '@/api/Inv/Inv_AdjustSvc'
+import EnumSelect from '@/components/CF/EnumSelect'
+import CodeInput from '@/components/CF/CodeInput'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import TreeSelect from '@/components/CF/TreeSelect'
+import CommonSelect from '@/components/CF/CommonSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+import SkuSelect from '@/components/Bas/SkuSelect'
+import InvChoose from '@/components/Inv/InvChoose'
+import TraySelect from '@/components/Bas/TraySelect'
+export default {
+  components: {
+    MainSvc,
+    CodeInput,
+    EnumSelect,
+    StorerSelect,
+    TreeSelect,
+    CommonSelect,
+    LocSelect,
+    SkuSelect,
+    InvChoose,
+    TraySelect
+  },
+  props: {},
+  data() {
+    return {
+      title: '新建',
+      layout: { labelCol: { xs: { span: 24 }, sm: { span: 6 } }, wrapperCol: { xs: { span: 24 }, sm: { span: 14 } } },
+      rules: {
+        Name: [{ required: true, message: '必填' }],
+        Code: [{ required: true, message: '必填' }]
+      },
+      visible: false,
+      loading: false,
+      entity: {},
+      curDetailIndex: 0,
+      columns: [
+        { title: '物料编码', dataIndex: 'Sku.Code' },
+        { title: '物料名称', dataIndex: 'Sku.Name' },
+        { title: '批次', dataIndex: 'Lot.Code', width: 120 },
+        { title: '库位', dataIndex: 'Loc.Code', width: 120 },
+        { title: '托盘', dataIndex: 'Tray.Code', width: 120 },
+        { title: '调整数量', dataIndex: 'QtyAdj', width: 100, scopedSlots: { customRender: 'QtyAdj' } },
+        { title: '调整类型', dataIndex: 'Type', width: 150, scopedSlots: { customRender: 'Type' } },
+        { title: '操作', dataIndex: 'action', width: 100, scopedSlots: { customRender: 'action' } }
+      ]
+    }
+  },
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    }),
+    adjustDetail() {
+      return this.entity.AdjustDetail
+    }
+  },
+  created() { },
+  methods: {
+    moment,
+    ...mapActions({ getConfig: 'getConfig' }),
+    init() {
+      this.loading = false
+      this.visible = true
+      this.entity = {
+        AdjustDetail: [],
+        Id: '', WhseId: this.defaultWhseId, StorerId: this.defaultStorerId, Code: '',
+        DocDate: moment().format('YYYY-MM-DD'), AdjDate: moment().format('YYYY-MM-DD'), Remark: '', Status: 'Active'
+      }
+      this.$nextTick(() => {
+        this.$refs.form.clearValidate()
+      })
+    },
+    openForm(id, title) {
+      this.title = title || (id ? '新建' : '修改')
+      this.init()
+      if (id) {
+        MainSvc.Get(id).then(resJson => {
+          this.entity = resJson.Data
+        })
+      } else {
+        this.$nextTick(() => {
+          this.$refs.codeInput.Generate()
+        })
+      }
+    },
+    handleAdd() {
+      this.$refs.invChoose.choose({
+        StorerId: this.entity.StorerId
+      })
+    },
+    handlerInvChoose(selectRows) {
+      if (selectRows.length === 0) return
+      var inv = selectRows[0]
+      console.log('handlerInvChoose', inv)
+      if (inv.StorerId !== this.entity.StorerId) {
+        this.$message.error('选择的货主不匹配')
+        return
+      }
+      this.curDetailIndex += 1
+      var detail = {
+        Id: `new_${this.curDetailIndex}`, WhseId: this.defaultWhseId, StorerId: this.entity.StorerId, AdjustId: this.entity.Id,
+        SkuId: inv.SkuId, LotId: inv.LotId, LocId: inv.LocId, TrayId: inv.TrayId, QtyAdj: 0, Type: '',
+        Sku: inv.Sku, Lot: inv.Lot, Loc: inv.Loc, Tray: inv.Tray
+      }
+      console.log(detail)
+      this.entity.AdjustDetail.push(detail)
+    },
+    handlePrint() { },
+    handleAdjust() { },
+    handleSubmit() {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          this.$message.error('数据验证失败')
+          return
+        }
+        const list = this.entity.AdjustDetail.map(detail => Object.assign({}, detail))
+        const validMsg = []
+        list.forEach(detail => {
+          if (detail.QtyAdj === 0) validMsg.push(`调整数量不能为0`)
+          detail.Sku = null
+          detail.Lot = null
+          detail.Loc = null
+          detail.Tray = null
+          if (!detail.TrayId) detail.TrayId = null // 如果没有托盘
+        })
+        if (validMsg.length > 0) {
+          this.$message.error((h) => { return (<a-list size="small" split={false}>{validMsg.map(m => { return (<a-list-item>{m}</a-list-item>) })}</a-list>) })
+          return
+        }
+        const postData = Object.assign({}, this.entity)
+        postData.AdjustDetail = list
+        this.loading = true
+        MainSvc.Save(postData).then(result => {
+          this.loading = false
+          if (result.Success) {
+            this.$message.success(result.Msg)
+            this.visible = false
+            this.$emit('Success')
+          } else {
+            this.$message.error(result.Msg)
+          }
+        })
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/Inv/Inv_Adjust/List.vue
@@ -0,0 +1,176 @@
+<template>
+  <a-card :bordered="false">
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="48">
+          <a-col :md="6" :sm="24">
+            <a-form-item label="货主">
+              <StorerSelect v-model="queryParam.StorerId" :type="['Storer']" placeholder="货主"></StorerSelect>
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="24">
+            <a-form-item label="编码">
+              <a-input v-model="queryParam.Keyword" placeholder="编码" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="24">
+            <a-form-item label="制单日期">
+              <a-range-picker :value="[queryParam.DocDateStart,queryParam.DocDateEnd]" format="YYYY-MM-DD" @change="onDateChange" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="24">
+            <span class="table-page-search-submitButtons">
+              <a-button type="primary" v-action:Query @click="()=>{this.$refs.table.refresh()}">查询</a-button>
+              <a-button style="margin-left: 8px" @click="resetSearchForm()">重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+
+    <div class="table-operator">
+      <a-button type="primary" v-action:Add icon="plus" @click="handleAdd">新建</a-button>
+      <a-button type="primary" v-action:Delete icon="delete" @click="handleDelete()">批量删除</a-button>
+    </div>
+
+    <s-table ref="table" size="default" rowKey="Id" :columns="columns" :data="loadData" :rowSelection="rowSelection" showPagination="auto">
+      <template slot="Status" slot-scope="text">
+        <EnumName code="Inv_Adjust_Status" :value="text"></EnumName>
+      </template>
+      <span slot="action" slot-scope="text, record">
+        <template>
+          <a v-action:Update @click="handleEdit(record)">修改</a>
+          <a-divider v-action:Delete type="vertical" />
+          <a v-action:Delete @click="handleDelete([record])">删除</a>
+        </template>
+      </span>
+    </s-table>
+    <EditForm ref="editForm" @Success="()=>{this.$refs.table.refresh()}"></EditForm>
+  </a-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import moment from 'moment'
+import { STable } from '@/components'
+import MainSvc from '@/api/Inv/Inv_AdjustSvc'
+import EditForm from './Edit'
+import EnumSelect from '@/components/CF/EnumSelect'
+import EnumName from '@/components/CF/EnumName'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import SkuSelect from '@/components/Bas/SkuSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+
+const columns = [
+  { title: '货主名称', dataIndex: 'Storer.Name' },
+  { title: '货主编号', dataIndex: 'Storer.Code' },
+  { title: '编号', dataIndex: 'Code', sorter: true },
+  { title: '制单日期', dataIndex: 'DocDate', sorter: true, customRender: (value) => { return moment(value).format('yyyy-MM-DD') } },
+  { title: '调整日期', dataIndex: 'AdjDate', sorter: true, customRender: (value) => { return moment(value).format('yyyy-MM-DD') } },
+  { title: '状态', dataIndex: 'Status', scopedSlots: { customRender: 'Status' } },
+  { title: '修改时间', dataIndex: 'ModifyTime', sorter: true, customRender: (value) => { return moment(value).format('yyyy-MM-DD') } },
+  { title: '操作', dataIndex: 'action', width: 200, scopedSlots: { customRender: 'action' } }
+]
+
+export default {
+  components: {
+    STable,
+    MainSvc,
+    EnumSelect,
+    EnumName,
+    EditForm,
+    StorerSelect,
+    SkuSelect,
+    LocSelect
+  },
+  data() {
+    this.columns = columns
+    return {
+      // create model
+      visible: false,
+      confirmLoading: false,
+      mdl: null,
+      // 高级搜索 展开/关闭
+      advanced: false,
+      // 查询参数
+      queryParam: { WhseId: '', Keyword: '', StorerId: '', DocDateStart: null, DocDateEnd: null },
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        this.queryParam.WhseId = this.defaultWhseId
+        var _query = Object.assign({}, { ...this.queryParam })
+        for (const key in _query) {
+          if (moment.isMoment(_query[key])) {
+            _query[key] = _query[key].format('YYYY-MM-DD')
+          }
+        }
+        const requestParameters = Object.assign({ sortField: 'Code', sortOrder: 'desc', Search: _query }, parameter)
+        console.log('loadData request parameters:', requestParameters)
+        return MainSvc.GetPage(requestParameters)
+      },
+      selectedRowKeys: [],
+      selectedRows: []
+    }
+  },
+  filters: {
+  },
+  created() {
+    this.resetSearchForm()
+  },
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    }),
+    rowSelection() {
+      return {
+        selectedRowKeys: this.selectedRowKeys,
+        onChange: this.onSelectChange
+      }
+    }
+  },
+  methods: {
+    moment,
+    handleAdd() {
+      this.mdl = null
+      this.visible = true
+      this.$refs.editForm.openForm(null, '新增')
+    },
+    onDateChange(dates, dateStrings) {
+      this.queryParam.DocDateStart = dates[0]
+      this.queryParam.DocDateEnd = dates[1]
+    },
+    handleEdit(record) {
+      this.visible = true
+      this.mdl = { ...record }
+      this.$refs.editForm.openForm(record.Id, '修改')
+    },
+    onSelectChange(selectedRowKeys, selectedRows) {
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
+    },
+    resetSearchForm() {
+      this.queryParam = { WhseId: this.defaultWhseId, Keyword: '', StorerId: this.defaultStorerId, DocDateStart: moment(), DocDateEnd: moment().add(1, 'days') }
+    },
+    handleDelete(rows) {
+      var thisObj = this
+      var ids = rows.map(value => value.Id)
+      this.$confirm({
+        title: '确认删除吗?',
+        onOk() {
+          return new Promise((resolve, reject) => {
+            MainSvc.Delete(ids).then(result => {
+              resolve()
+              if (result.Success) {
+                thisObj.$message.success('操作成功!')
+                thisObj.$refs.table.refresh()
+              } else {
+                thisObj.$message.error(result.Msg)
+              }
+            })
+          })
+        }
+      })
+    }
+  }
+}
+</script>
