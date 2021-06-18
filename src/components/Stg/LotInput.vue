94cd5130ca130c2fa79d708dcb965af4654ae789
Giant <liuju150@outlook.com>
2021-06-18 16:47:49 +0800

    按批属性与库位扩展上架

--- src/components/Stg/LotInput.vue
+++ src/components/Stg/LotInput.vue
@@ -3,15 +3,18 @@
     <a-input-number v-model="curVal" v-if="sku && sku.LotStg[`${name}Type`]==='Number'" style="width:100%" v-bind="$attrs" />
     <a-date-picker v-model="curVal" v-else-if="sku && sku.LotStg[`${name}Type`]==='Date'" valueFormat="YYYY-MM-DD" v-bind="$attrs" />
     <EnumSelect :code="sku.LotStg[`${name}Enum`]" v-model="curVal" v-else-if="sku && sku.LotStg[`${name}Type`]==='Enum'" style="width:100%" v-bind="$attrs"></EnumSelect>
+    <CommonSelect :table="sku.LotStg[`${name}Enum`]" v-model="curVal" v-else-if="sku && sku.LotStg[`${name}Type`]==='Select'" style="width:100%" v-bind="$attrs"></CommonSelect>
     <a-input v-else v-model="curVal" v-bind="$attrs" />
   </div>
 </template>
 
 <script>
 import EnumSelect from '@/components/CF/EnumSelect'
+import CommonSelect from '@/components/CF/CommonSelect'
 export default {
   components: {
-    EnumSelect
+    EnumSelect,
+    CommonSelect
   },
   props: {
     name: String,
