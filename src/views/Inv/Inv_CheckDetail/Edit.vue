242d340436e5fe075193793b580e6ace3d2d0fc6
Giant <liuju150@outlook.com>
2021-07-02 10:57:28 +0800

    盘点功能初始化

---
+++ src/views/Inv/Inv_CheckDetail/Edit.vue
@@ -0,0 +1,97 @@
+<template>
+  <a-modal :title="title" :width="640" :visible="visible" :confirmLoading="loading" @ok="handleSubmit" @cancel="()=>{this.visible=false}">
+    <a-spin :spinning="loading">
+      <a-form-model ref="form" :model="entity" :rules="rules" v-bind="layout">
+        <a-form-model-item label="编号" prop="Code">
+          <a-input v-model="entity.Code" autocomplete="off" />
+        </a-form-model-item>
+        <a-form-model-item label="名称" prop="Name">
+          <a-input v-model="entity.Name" autocomplete="off" />
+        </a-form-model-item>
+      </a-form-model>
+    </a-spin>
+  </a-modal>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import MainSvc from '@/api/Inv/Inv_CheckDetailSvc'
+import EnumSelect from '@/components/CF/EnumSelect'
+import CodeInput from '@/components/CF/CodeInput'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import TreeSelect from '@/components/CF/TreeSelect'
+import CommonSelect from '@/components/CF/CommonSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+import SkuSelect from '@/components/Bas/SkuSelect'
+export default {
+  components: {
+    MainSvc,
+    CodeInput,
+    EnumSelect,
+    StorerSelect,
+    TreeSelect,
+    CommonSelect,
+    LocSelect,
+    SkuSelect
+  },
+  props: {},
+  data() {
+    return {
+      title: '新建',
+      layout: { labelCol: { xs: { span: 24 }, sm: { span: 6 } }, wrapperCol: { xs: { span: 24 }, sm: { span: 14 } } },
+      rules: {
+        Name: [{ required: true, message: '必填' }],
+        Code: [{ required: true, message: '必填' }]
+      },
+      visible: false,
+      loading: false,
+      entity: {}
+    }
+  },
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    })
+  },
+  created() { },
+  methods: {
+    ...mapActions({ getConfig: 'getConfig' }),
+    init() {
+      this.loading = false
+      this.visible = true
+      this.entity = { Id: '', WhseId: this.defaultWhseId, Code: '', Name: '' }
+      this.$nextTick(() => {
+        this.$refs.form.clearValidate()
+      })
+    },
+    openForm(id, title) {
+      this.title = title || (id ? '新建' : '修改')
+      this.init()
+      if (id) {
+        MainSvc.Get(id).then(resJson => {
+          this.entity = resJson.Data
+        })
+      }
+    },
+    handleSubmit() {
+      this.$refs['form'].validate(valid => {
+        if (!valid) {
+          return
+        }
+        this.loading = true
+        MainSvc.Save(this.entity).then(result => {
+          this.loading = false
+          if (result.Success) {
+            this.$message.success(result.Msg)
+            this.visible = false
+            this.$emit('Success')
+          } else {
+            this.$message.error(result.Msg)
+          }
+        })
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/Inv/Inv_CheckDetail/List.vue
@@ -0,0 +1,153 @@
+<template>
+  <a-card :bordered="false">
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="48">
+          <a-col :md="6" :sm="24">
+            <a-form-item label="关键字">
+              <a-input v-model="queryParam.Keyword" placeholder="关键字" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="6" :sm="24">
+            <span class="table-page-search-submitButtons">
+              <a-button type="primary" v-action:Query @click="()=>{this.$refs.table.refresh()}">查询</a-button>
+              <a-button style="margin-left: 8px" @click="resetSearchForm()">重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+
+    <div class="table-operator">
+      <a-button type="primary" v-action:Add icon="plus" @click="handleAdd">新建</a-button>
+      <a-button type="primary" v-action:Delete icon="delete" @click="handleDelete()">批量删除</a-button>
+    </div>
+
+    <s-table ref="table" size="default" rowKey="Id" :columns="columns" :data="loadData" :rowSelection="rowSelection" showPagination="auto">
+      <span slot="action" slot-scope="text, record">
+        <template>
+          <a v-action:Update @click="handleEdit(record)">修改</a>
+          <a-divider v-action:Delete type="vertical" />
+          <a v-action:Delete @click="handleDelete([record])">删除</a>
+        </template>
+      </span>
+    </s-table>
+    <EditForm ref="editForm" @Success="()=>{this.$refs.table.refresh()}"></EditForm>
+  </a-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import moment from 'moment'
+import { STable } from '@/components'
+import MainSvc from '@/api/Inv/Inv_CheckDetailSvc'
+import EditForm from './Edit'
+import EnumSelect from '@/components/CF/EnumSelect'
+import EnumName from '@/components/CF/EnumName'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import SkuSelect from '@/components/Bas/SkuSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+
+const columns = [
+  { title: '编号', dataIndex: 'Code', sorter: true },
+  { title: '名称', dataIndex: 'Name', sorter: true },
+  { title: '修改时间', dataIndex: 'ModifyTime', sorter: true, customRender: (value) => { return moment(value).format('yyyy-MM-DD') } },
+  { title: '操作', dataIndex: 'action', width: '200px', scopedSlots: { customRender: 'action' } }
+]
+
+export default {
+  components: {
+    STable,
+    MainSvc,
+    EnumSelect,
+    EnumName,
+    EditForm,
+    StorerSelect,
+    SkuSelect,
+    LocSelect
+  },
+  data() {
+    this.columns = columns
+    return {
+      // create model
+      visible: false,
+      confirmLoading: false,
+      mdl: null,
+      // 高级搜索 展开/关闭
+      advanced: false,
+      // 查询参数
+      queryParam: { WhseId: '', Keyword: '' },
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        this.queryParam.WhseId = this.defaultWhseId
+        var _query = Object.assign({}, { ...this.queryParam })
+        for (const key in _query) {
+          if (moment.isMoment(_query[key])) {
+            _query[key] = _query[key].format('YYYY-MM-DD')
+          }
+        }
+        const requestParameters = Object.assign({ sortField: 'ModifyTime', sortOrder: 'desc', Search: _query }, parameter)
+        console.log('loadData request parameters:', requestParameters)
+        return MainSvc.GetPage(requestParameters)
+      },
+      selectedRowKeys: [],
+      selectedRows: []
+    }
+  },
+  filters: {
+  },
+  created() {},
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    }),
+    rowSelection() {
+      return {
+        selectedRowKeys: this.selectedRowKeys,
+        onChange: this.onSelectChange
+      }
+    }
+  },
+  methods: {
+    moment,
+    handleAdd() {
+      this.mdl = null
+      this.visible = true
+      this.$refs.editForm.openForm(null, '新增')
+    },
+    handleEdit(record) {
+      this.visible = true
+      this.mdl = { ...record }
+      this.$refs.editForm.openForm(record.Id, '修改')
+    },
+    onSelectChange(selectedRowKeys, selectedRows) {
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
+    },
+    resetSearchForm() {
+      this.queryParam = { WhseId: this.defaultWhseId, Keyword: '' }
+    },
+    handleDelete(rows) {
+      var thisObj = this
+      var ids = rows.map(value => value.Id)
+      this.$confirm({
+        title: '确认删除吗?',
+        onOk() {
+          return new Promise((resolve, reject) => {
+            MainSvc.Delete(ids).then(result => {
+              resolve()
+              if (result.Success) {
+                thisObj.$message.success('操作成功!')
+                thisObj.$refs.table.refresh()
+              } else {
+                thisObj.$message.error(result.Msg)
+              }
+            })
+          })
+        }
+      })
+    }
+  }
+}
+</script>
