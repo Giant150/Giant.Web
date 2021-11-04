484a79c6f0b539667a1ba4dd956aa022bfdafc53
Giant <liuju150@outlook.com>
2021-11-04 17:23:57 +0800

    Log:日志查询模块

---
+++ src/api/Sys/Sys_LogSvc.js
@@ -0,0 +1,29 @@
+import request from '@/utils/request'
+/**
+ * Sys_Log API接口服务
+ */
+export default {
+    /**
+     * 请求分页数据
+     * @param {Object} parameter 查询参数
+     * @returns 分页数据
+     */
+    GetPage(parameter) {
+        return request({
+            url: '/api/Sys_Log/GetPage',
+            method: 'post',
+            data: parameter
+        })
+    },
+    /**
+     * 获取单个实体数据
+     * @param {string} id 主键
+     * @returns 实体数据
+     */
+    Get(id) {
+        return request({
+            url: '/api/Sys_Log/Get?id=' + id,
+            method: 'get'
+        })
+    }
+}
