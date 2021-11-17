23dafb650e63f83b122e7f68ab0fd159eb56c642
Giant <liuju150@outlook.com>
2021-11-17 12:40:23 +0800

    Serial:序列号功能

---
+++ src/views/Inv/Inv_Serial/List.vue
@@ -0,0 +1,200 @@
+<template>
+  <a-card :bordered="false">
+    <div class="table-page-search-wrapper">
+      <a-form layout="inline">
+        <a-row :gutter="48">
+          <a-col :md="4" :sm="24">
+            <a-form-item label="货主">
+              <StorerSelect v-model="queryParam.StorerId" :type="['Storer']"></StorerSelect>
+            </a-form-item>
+          </a-col>
+          <a-col :md="4" :sm="24">
+            <a-form-item label="物料">
+              <SkuSelect v-model="queryParam.SkuId" :storer="queryParam.StorerId" @select="(val,sku)=>{this.querySku=sku}"></SkuSelect>
+            </a-form-item>
+          </a-col>
+          <a-col :md="4" :sm="24">
+            <a-form-item label="关键字">
+              <a-input v-model="queryParam.Keyword" placeholder="序号号" />
+            </a-form-item>
+          </a-col>
+          <a-col :md="4" :sm="24">
+            <a-form-item label="来源">
+              <EnumSelect code="Inv_Serial_FromType" v-model="queryParam.FromType"></EnumSelect>
+            </a-form-item>
+          </a-col>
+          <a-col :md="4" :sm="24">
+            <a-form-item label="状态">
+              <EnumSelect code="Inv_Serial_Status" v-model="queryParam.Status"></EnumSelect>
+            </a-form-item>
+          </a-col>
+          <a-col :md="4" :sm="24">
+            <span class="table-page-search-submitButtons">
+              <a-button type="primary" v-action:Query @click="()=>{this.$refs.table.refresh()}">查询</a-button>
+              <a-button style="margin-left: 8px" @click="resetSearchForm()">重置</a-button>
+            </span>
+          </a-col>
+        </a-row>
+      </a-form>
+    </div>
+
+    <div class="table-operator">
+      <a-button type="primary" v-action:Add icon="plus" @click="handleAdd">新建</a-button>
+      <a-button type="primary" v-action:Delete icon="delete" @click="handleDelete()">批量删除</a-button>
+    </div>
+
+    <s-table ref="table" size="default" rowKey="Id" :columns="columns" :data="loadData" :rowSelection="rowSelection" showPagination="auto" :scroll="{ x: 2000 }">
+      <template slot="FromType" slot-scope="text">
+        <EnumName code="Inv_Serial_FromType" :value="text"></EnumName>
+      </template>
+      <template slot="Status" slot-scope="text">
+        <EnumName code="Inv_Serial_Status" :value="text"></EnumName>
+      </template>
+      <span slot="action" slot-scope="text, record">
+        <template>
+          <a v-action:Update @click="handleEdit(record)">修改</a>
+          <a-divider v-action:Delete type="vertical" />
+          <a v-action:Delete @click="handleDelete([record])">删除</a>
+          <a-divider v-action:Print type="vertical" />
+          <a v-action:Print @click="handlePrint(record)">打印</a>
+        </template>
+      </span>
+    </s-table>
+    <EditForm ref="editForm" @Success="()=>{this.$refs.table.refresh()}"></EditForm>
+  </a-card>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import moment from 'moment'
+import { STable } from '@/components'
+import MainSvc from '@/api/Inv/Inv_SerialSvc'
+import EditForm from './Edit'
+import EnumSelect from '@/components/CF/EnumSelect'
+import EnumName from '@/components/CF/EnumName'
+import StorerSelect from '@/components/Bas/StorerSelect'
+import SkuSelect from '@/components/Bas/SkuSelect'
+import LocSelect from '@/components/Bas/LocSelect'
+
+const columns = [
+  { title: '货主', dataIndex: 'Storer.Name', width: 120, fixed: 'left' },
+  { title: '序列号', dataIndex: 'Code', sorter: true, width: 200, fixed: 'left' },
+  { title: '物料编号', dataIndex: 'Sku.Code', width: 200 },
+  { title: '物料名称', dataIndex: 'Sku.Name' },
+  { title: '物料规格', dataIndex: 'Sku.Spec' },
+  { title: '批次号', dataIndex: 'Lot.Code', width: 120 },
+  { title: '来源', dataIndex: 'FromType', width: 120, scopedSlots: { customRender: 'FromType' } },
+  { title: '关联单号', width: 200, customRender: (value, record) => { return record.Receipt?.Code || record.Order?.Code } },
+  { title: '状态', dataIndex: 'Status', width: 120, scopedSlots: { customRender: 'Status' } },
+  { title: '修改时间', dataIndex: 'ModifyTime', width: 120, sorter: true, customRender: (value) => { return moment(value).format('yyyy-MM-DD') } },
+  { title: '操作', dataIndex: 'action', width: 150, fixed: 'right', scopedSlots: { customRender: 'action' } }
+]
+
+export default {
+  components: {
+    STable,
+    MainSvc,
+    EnumSelect,
+    EnumName,
+    EditForm,
+    StorerSelect,
+    SkuSelect,
+    LocSelect
+  },
+  data() {
+    this.columns = columns
+    return {
+      // create model
+      visible: false,
+      confirmLoading: false,
+      mdl: null,
+      // 高级搜索 展开/关闭
+      advanced: false,
+      // 查询参数
+      queryParam: { WhseId: '', Keyword: '', StorerId: '', SkuId: '', FromType: undefined, Status: undefined },
+      // 加载数据方法 必须为 Promise 对象
+      loadData: parameter => {
+        this.queryParam.WhseId = this.defaultWhseId
+        var _query = Object.assign({}, { ...this.queryParam })
+        for (const key in _query) {
+          if (moment.isMoment(_query[key])) {
+            _query[key] = _query[key].format('YYYY-MM-DD')
+          }
+        }
+        const requestParameters = Object.assign({ sortField: 'Code', sortOrder: 'desc', Search: _query }, parameter)
+        console.log('loadData request parameters:', requestParameters)
+        return MainSvc.GetPage(requestParameters)
+      },
+      selectedRowKeys: [],
+      selectedRows: []
+    }
+  },
+  filters: {
+  },
+  created() {
+    this.resetSearchForm()
+  },
+  computed: {
+    ...mapGetters({
+      defaultWhseId: 'whseId',
+      defaultStorerId: 'storerId'
+    }),
+    rowSelection() {
+      return {
+        selectedRowKeys: this.selectedRowKeys,
+        onChange: this.onSelectChange
+      }
+    }
+  },
+  methods: {
+    moment,
+    handleAdd() {
+      this.mdl = null
+      this.visible = true
+      this.$refs.editForm.openForm(null, '新增')
+    },
+    handleEdit(record) {
+      this.visible = true
+      this.mdl = { ...record }
+      this.$refs.editForm.openForm(record.Id, '修改')
+    },
+    onSelectChange(selectedRowKeys, selectedRows) {
+      this.selectedRowKeys = selectedRowKeys
+      this.selectedRows = selectedRows
+    },
+    resetSearchForm() {
+      this.queryParam = { WhseId: this.defaultWhseId, Keyword: '', StorerId: this.defaultStorerId, SkuId: '', FromType: undefined, Status: undefined }
+    },
+    handleDelete(rows) {
+      var thisObj = this
+      var ids = rows.map(value => value.Id)
+      this.$confirm({
+        title: '确认删除吗?',
+        onOk() {
+          return new Promise((resolve, reject) => {
+            MainSvc.Delete(ids).then(result => {
+              resolve()
+              if (result.Success) {
+                thisObj.$message.success('操作成功!')
+                thisObj.$refs.table.refresh()
+              } else {
+                thisObj.$message.error(result.Msg)
+              }
+            })
+          })
+        }
+      })
+    },
+    handlePrint(record) {
+      var printData = { Code: record.Sku.Code, Name: record.Sku.Name, Spec: record.Sku.Spec, Model: '', LotNo: record.Lot?.Code, SerialNo: record.Code, WorkNo: record.Lot01 }
+      this.$http.post(`${process.env.VUE_APP_API_PRINT_URL}/Print`, printData).then(print => {
+        if (print.Success) {
+          this.$message.info(print.Msg)
+        } else {
+          this.$message.error(print.Msg)
+        }
+      })
+    }
+  }
+}
+</script>
