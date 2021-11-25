c7dc86f2f404236368732c7f7cb7846346608fcf
Giant <liuju150@outlook.com>
2021-11-25 10:00:48 +0800

    EnumColor:组件增加颜色配置

--- src/views/CF/CF_EnumItem/Edit.vue
+++ src/views/CF/CF_EnumItem/Edit.vue
@@ -11,6 +11,9 @@
         <a-form-model-item label="排序" prop="Sort">
           <a-input-number v-model="entity.Sort" style="width:100%"></a-input-number>
         </a-form-model-item>
+        <a-form-model-item label="颜色" prop="Color">
+          <EnumSelect code="Color" v-model="entity.Color" :color="true"></EnumSelect>
+        </a-form-model-item>
         <a-form-model-item label="配置参数" prop="Config">
           <a-textarea v-model="entity.Config" autocomplete="off"></a-textarea>
         </a-form-model-item>
@@ -26,10 +29,14 @@
 import { mapActions, mapGetters } from 'vuex'
 import MainSvc from '@/api/CF/CF_EnumItemSvc'
 import EnumSvc from '@/api/CF/CF_EnumSvc'
+import EnumSelect from '@/components/CF/EnumSelect'
+import EnumName from '@/components/CF/EnumName'
 export default {
   components: {
     MainSvc,
-    EnumSvc
+    EnumSvc,
+    EnumSelect,
+    EnumName
   },
   props: {},
   data() {
@@ -57,7 +64,7 @@ export default {
     init() {
       this.loading = false
       this.visible = true
-      this.entity = { Id: '', WhseId: this.defaultWhseId, EnumId: '', Code: '', Name: '', Sort: 1, Config: undefined, Remark: undefined, IsSystem: false }
+      this.entity = { Id: '', WhseId: this.defaultWhseId, EnumId: '', Code: '', Name: '', Sort: 1, Color: undefined, Config: undefined, Remark: undefined, IsSystem: false }
       this.$nextTick(() => {
         this.$refs.form.clearValidate()
       })
