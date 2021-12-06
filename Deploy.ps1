6df09f94c920694de8e0d5626221d5e97c5ae7a5
Giant <liuju150@outlook.com>
2021-12-06 19:50:14 +0800

    deploy

--- Deploy.ps1
+++ Deploy.ps1
@@ -1,2 +1,2 @@
-. .\DeployModule\Start-Deploy.ps1
-Start-Deploy -ComputerName 192.171.1.5 -WebSiteName WMSWeb -WebSitePort 8051 -ScriptBlock { npm run build:live }
+#https://github.com/ZEQP/ZEQP-PSDeploy
+Start-Deploy -ComputerName 192.171.1.5 -WebSiteName WMSWeb -WebSitePort 8051 -ScriptBlock { npm run build:live } -OutputPath .\dist\
