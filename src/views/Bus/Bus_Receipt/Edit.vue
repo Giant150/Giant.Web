d0016bf1c9eab5400089c15568e031163ebd9a2d
Giant <liuju150@outlook.com>
2021-12-20 15:50:07 +0800

    Search: 托盘选择组件优化

--- src/views/Bus/Bus_Receipt/Edit.vue
+++ src/views/Bus/Bus_Receipt/Edit.vue
@@ -86,7 +86,7 @@
           <LocSelect v-model="record.LocId" size="small" :disabled="!!record.LotId"></LocSelect>
         </template>
         <template slot="TrayId" slot-scope="text, record">
-          <TraySelect v-model="record.TrayId" :type="record.Sku?record.Sku.TrayTypeId:''" size="small" allowClear :disabled="!!record.LotId"></TraySelect>
+          <TraySelect v-model="record.TrayId" size="small" allowClear :disabled="!!record.LotId"></TraySelect>
         </template>
         <template slot="Lot01" slot-scope="text, record">
           <LotInput name="Lot01" :sku="record.Sku" v-model="record.Lot01" :disabled="!!record.LotId" size="small"></LotInput>
