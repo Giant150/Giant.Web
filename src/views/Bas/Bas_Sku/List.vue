823c193e0c13ab4d49014edacbdb5fe0c68d21ec
Giant <liuju150@outlook.com>
2021-12-27 14:14:25 +0800

    sku order

--- src/views/Bas/Bas_Sku/List.vue
+++ src/views/Bas/Bas_Sku/List.vue
@@ -149,7 +149,7 @@ export default {
       // 加载数据方法 必须为 Promise 对象
       loadData: parameter => {
         this.queryParam.WhseId = this.defaultWhseId
-        const requestParameters = Object.assign({ sortField: 'ModifyTime', sortOrder: 'desc', Search: { ...this.queryParam } }, parameter)
+        const requestParameters = Object.assign({ sortField: 'Code', sortOrder: 'asc', Search: { ...this.queryParam } }, parameter)
         console.log('loadData request parameters:', requestParameters)
         return MainSvc.GetPage(requestParameters)
       },
